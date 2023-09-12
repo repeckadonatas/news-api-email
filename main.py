857b60ee06a1ac4c590b3ff26aa0f4236dc9d6d5
repeckadonatas <repeckadonatas@yahoo.comm>
2023-09-12 22:25:18 +0300

    Saving title and description of articles into a txt file and sending it to a provided email address.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = '9886cf5322904eb0af66f4133917f69b'
 url = ('https://newsapi.org/v2/everything?q=tesla&'
@@ -11,8 +12,12 @@ request = requests.get(url)
 # Get data as dictionary
 content = request.json()
 
-# Access the titles and descriptions of articles
-for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+# Creating a txt file to store title and description of articles
+filename = 'message-article.txt'
+with open(filename, 'w+', encoding='utf-8') as file:
+    for article in content['articles']:
+        title = file.writelines(article['title'])
+        description = file.writelines(article['description'])
 
+# Sending a txt file to a provided email address
+send_email(filename)
