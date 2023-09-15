aff2fe28de22f89dbca14629e1092e477ca0ed16
repeckadonatas <repeckadonatas@yahoo.comm>
2023-09-15 20:59:24 +0300

    Small changes to make a text more readable

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 from send_email import send_email
 
 api_key = '9886cf5322904eb0af66f4133917f69b'
-url = ('https://newsapi.org/v2/everything?q=tesla&'
-       'from=2023-08-12&sortBy=publishedAt&'
+url = ('https://newsapi.org/v2/everything?q=tesla'
+       '&from=2023-08-15&sortBy=publishedAt&'
        'apiKey=9886cf5322904eb0af66f4133917f69b')
 
 # Make a request
@@ -16,8 +16,9 @@ content = request.json()
 filename = 'message-article.txt'
 with open(filename, 'w+', encoding='utf-8') as file:
     for article in content['articles']:
-        title = file.writelines(article['title'])
-        description = file.writelines(article['description'])
+        if article['title'] is not None:
+            title = file.writelines(article['title'] + '\n')
+            description = file.writelines(article['description'] + 2 * '\n')
 
 # Sending a txt file to a provided email address
 send_email(filename)
